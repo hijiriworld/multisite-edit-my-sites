db354d48c81e2809cb363a28b1163a5b299b97df
Satoshi SHODA <hijiriworld@gmail.com>
2019-06-10 17:05:08 +0900

    add role reset.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 (function($){
 	$('#hems_all_apply').on('click', function () {
 		var $val = $('#hems_all_select').val();
+		if( $val == 'none' ) return false;
 		$("[name^='hems_check']:checked").each( function(i, elm ) {
 			var $input = $(elm);
 			$row = $input.closest('tr');
 			$row.find("[name^='users_sites']").val($val);
-			
 		});
 	});
 })(jQuery)
